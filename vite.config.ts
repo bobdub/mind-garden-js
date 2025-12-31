b0c7bf4546b143ee46c0811ec73c5b8771ddbf6e
bobdub <robertawessex@gmail.com>
2025-12-31 21:42:47 +0200

    Fix Pages build output directory

--- vite.config.ts
+++ vite.config.ts
@@ -12,8 +12,8 @@ export default defineConfig(({ mode }) => {
     base: basePath,
     publicDir: false,
     build: {
-      outDir: path.resolve(__dirname),
-      emptyOutDir: false,
+      outDir: path.resolve(__dirname, "dist"),
+      emptyOutDir: true,
     },
     server: {
       host: "::",
